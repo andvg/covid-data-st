5fd6052f55ccbbaa0e5770f90595d16d0bd7c02c
Andrea V <33574513+andvg@users.noreply.github.com>
2022-01-21 00:42:57 +0100

    dev

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.json(data)
 for i in data:
     st.write(i)
 
-df = pd.DataFrame(data.items()).transpose()
+df = pd.DataFrame(data.items())
 #df.columns = ltA.iloc[0]
 #df = df.drop([0])
 #df = df.reset_index(drop=True)
