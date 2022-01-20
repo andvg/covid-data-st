a0988f05e027c10abd5663d2c667645f812a84fd
Andrea V <33574513+andvg@users.noreply.github.com>
2022-01-21 00:10:13 +0100

    dev

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
+import requests as r
 
-st.write('ok!')
+url = 'https://covid-api.mmediagroup.fr/v1/cases?country=Italy'
+
+data = r.get(url)
+
+st.json(data)
